072f469bdece4fc1fbcd796c7869bf732bdf52a7
Erik Töger <erik.toger@gmail.com>
2023-08-07 21:32:37 +0200

    f#: Bird Watcher

---
+++ fsharp/bird-watcher/BirdWatcher.fs
@@ -0,0 +1,21 @@
+module BirdWatcher
+
+let lastWeek: int[] = [| 0; 2; 5; 3; 7; 8; 4 |]
+
+let yesterday (counts: int[]) : int = counts[5]
+
+let total (counts: int[]) : int = counts |> Array.sum
+
+let dayWithoutBirds (counts: int[]) : bool =
+    counts |> Array.exists (fun elm -> elm = 0)
+
+let incrementTodaysCount (counts: int[]) : int[] =
+    counts[6] <- counts[6] + 1
+    counts
+
+let unusualWeek (counts: int[]) : bool =
+    match counts with
+    | [| _; 0; _; 0; _; 0; _ |] -> true
+    | [| 5; _; 5; _; 5; _; 5 |] -> true
+    | [| _; 10; _; 10; _; 10; _ |] -> true
+    | _ -> false
